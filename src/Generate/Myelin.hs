fe08ccc3cad7c33b62605575439c7da02f2082e3
Jens Egholm <jensegholm@protonmail.com>
2018-10-29 13:39:40 +0100

    Integrated with latest python generation backend

--- src/Generate/Myelin.hs
+++ src/Generate/Myelin.hs
@@ -5,9 +5,13 @@ import Control.Lens
 import Control.Monad (sequence_)
 import Control.Monad.Identity
 import Control.Monad.State.Lazy
-import Data.Aeson
-import Data.ByteString.Lazy
-import Myelin.SNN
+import Data.ByteString.Lazy.Char8
+import Myelin.Model
+import Myelin.Neuron
+import Myelin.SNN hiding (Hidden, Output, Input)
+import qualified Myelin.SNN as SNN (PopulationVisibility (Input, Output, Hidden))
+
+import Myelin.PyNN.PyNN
 
 import AST
 
@@ -15,50 +19,51 @@ data Target
   = Simulation
   | Hardware
 
+data NetworkVisibility
+  = InputOutput
+  | Input
+  | Output
+  | Hidden
+
 compile :: Target -> Term -> ByteString
 compile target term =
-  let ((input, output), rawBlock) = runState (compile' term) initialBlockState
-      block = rawBlock & outputs .~ output & inputs .~ input
-      network = Network [block]
-      myelinTarget = case target of
-        Simulation -> Nest 0 0
-        Hardware -> BrainScaleS 0 0
-      task = Task myelinTarget network 0
-  in encode task
+  let network = execState (compile' term InputOutput) initialNetwork
+      preample = PyNNPreample "nest.pyNN" ""
+  in  pack $ either Prelude.id Prelude.id $ translate network preample
 
-compile' :: Term -> SNN ([Node], [Node]) Identity
-compile' (TmNet l r) = do  
-  p1 <- lifPopulation l
-  p2 <- lifPopulation r
-  projection (AllToAll 1.0 False) (Static Excitatory) p1 p2 
+compile' :: Term -> NetworkVisibility -> SNN ([Node], [Node]) Identity
+compile' (TmNet l r) visibility = do
+  p1 <- lifPopulation l (toInput visibility)
+  p2 <- lifPopulation r (toOutput visibility)
+  projection (Static Excitatory (AllToAll (GaussianRandom 1 1))) p1 p2 
   return ([p1], [p2])
-compile' (TmSeq (TmPar lt lb) (TmPar rt rb)) = do
-  (lt1, lt2) <- compile' lt
-  (lb1, lb2) <- compile' lb
-  (rt1, rt2) <- compile' rt
-  (rb1, rb2) <- compile' rb
+compile' (TmSeq (TmPar lt lb) (TmPar rt rb)) visibility = do
+  (lt1, lt2) <- compile' lt (leftVisible visibility)
+  (lb1, lb2) <- compile' lb Hidden
+  (rt1, rt2) <- compile' rt Hidden
+  (rb1, rb2) <- compile' rb (rightVisible visibility)
   projectAll (lt2 ++ lb2) (rt1 ++ rb1)
   return (lt1 ++ lb1, rt2 ++ rb2)
-compile' (TmSeq (TmPar lt lb) r) = do
-  (lt1, lt2) <- compile' lt
-  (lb1, lb2) <- compile' lb
-  (r1, r2) <- compile' r
+compile' (TmSeq (TmPar lt lb) r) visibility = do
+  (lt1, lt2) <- compile' lt (leftVisible visibility)
+  (lb1, lb2) <- compile' lb Hidden
+  (r1, r2) <- compile' r (rightVisible visibility)
   projectAll (lt2 ++ lb2) r1
   return (lt1 ++ lb1, r2)
-compile' (TmSeq l (TmPar lt lb)) = do
-  (l1, l2) <- compile' l 
-  (lt1, lt2) <- compile' lt
-  (lb1, lb2) <- compile' lb
+compile' (TmSeq l (TmPar lt lb)) visibility = do
+  (l1, l2) <- compile' l (leftVisible visibility)
+  (lt1, lt2) <- compile' lt (rightVisible visibility)
+  (lb1, lb2) <- compile' lb (rightVisible visibility)
   projectAll l2 (lt1 ++ lb1)
   return (l1, lt2 ++ lb2)
-compile' (TmSeq l r) = do
-  (l1, l2) <- compile' l
-  (r1, r2) <- compile' r
+compile' (TmSeq l r) visibility = do
+  (l1, l2) <- compile' l (leftVisible visibility)
+  (r1, r2) <- compile' r (rightVisible visibility)
   projectAll l2 r1
   return (l1, r2)
-compile' (TmPar t b) = do
-  (t1, t2) <- compile' t
-  (b1, b2) <- compile' b
+compile' (TmPar t b) visibility = do
+  (t1, t2) <- compile' t (leftVisible visibility)
+  (b1, b2) <- compile' b (rightVisible visibility)
   return (t1 ++ b1, t2 ++ b2)
 
 projectAll :: [Node] -> [Node] -> SNN () Identity
@@ -66,9 +71,29 @@ projectAll lefts rights = do
   mapM_ (\n -> mapM_ (\m -> project n m) rights) lefts
 
 project :: Node -> Node -> SNN () Identity
-project left right = projection (AllToAll 1.0 False) (Static Excitatory) left right
+project left right = projection (Static Excitatory (AllToAll (GaussianRandom 1 1))) left right
 
-lifPopulation :: Int -> SNN Node Identity
-lifPopulation size = do
+lifPopulation :: Int -> PopulationVisibility -> SNN Node Identity
+lifPopulation size visibility = do
   label <- newId
-  population (show label) (fromIntegral size) if_cond_exp 
+  population (show label) (fromIntegral size) if_cond_exp visibility 
+
+leftVisible :: NetworkVisibility -> NetworkVisibility
+leftVisible InputOutput = Input
+leftVisible Input = Input
+leftVisible _ = Hidden
+
+rightVisible :: NetworkVisibility -> NetworkVisibility
+rightVisible InputOutput = Output
+rightVisible Output = Output
+rightVisible _ = Hidden
+
+toInput :: NetworkVisibility -> PopulationVisibility
+toInput Input = SNN.Input
+toInput InputOutput = SNN.Input
+toInput _ = SNN.Hidden
+
+toOutput :: NetworkVisibility -> PopulationVisibility
+toOutput Output = SNN.Output
+toOutput InputOutput = SNN.Output
+toOutput _ = SNN.Hidden
